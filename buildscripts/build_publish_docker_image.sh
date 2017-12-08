15106348d4a6d9849c7cdaca123105e8ccf68508
Rob Swartenbroekx <rob.s@sparkcentral.com>
2017-12-08 16:52:05 +0100

    fix publish docker script

--- buildscripts/build_publish_docker_image.sh
+++ buildscripts/build_publish_docker_image.sh
@@ -3,7 +3,7 @@ mkdir docker_workdir
 ./gradlew :application:distTar
 cp buildscripts/Dockerfile ./docker_workdir
 cd docker_workdir
-tar -xf ../application/build/distributions/application.tar
+tar -xf ../build/distributions/sparkcentral-rtm-demo-jwtgenerator.tar
 docker build -t sparkcentral/sparkcentral-rtm-demo-jwtgenerator:$TRAVIS_BUILD_NUMBER .
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker push sparkcentralinc/kafka-experiment:$TRAVIS_BUILD_NUMBER
