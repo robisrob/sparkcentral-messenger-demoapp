05657cc5a0af1cf1eb45e5e33c474870555d3381
Rob Swartenbroekx <rob.s@sparkcentral.com>
2017-12-09 10:03:56 +0100

    cleanup

--- buildscripts/build_publish_docker_image.sh
+++
@@ -1,9 +0,0 @@
-cd $HOME/build/sparkcentral/sparkcentral-rtm-demo-jwtgenerator
-mkdir docker_workdir
-./gradlew distTar
-cp buildscripts/Dockerfile ./docker_workdir
-cd docker_workdir
-tar -xf ../build/distributions/sparkcentral-rtm-demo-jwtgenerator.tar
-docker build -t sparkcentralinc/sparkcentral-rtm-demo-jwtgenerator:$TRAVIS_BUILD_NUMBER .
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker push sparkcentralinc/sparkcentral-rtm-demo-jwtgenerator:$TRAVIS_BUILD_NUMBER
